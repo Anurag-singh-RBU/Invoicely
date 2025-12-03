5167654d017361cd6ac2a53a3ffe11888d6ff237
Anurag-singh-RBU <anuragsingh260805@gmail.com>
2025-12-03 14:23:38 +0530

    hero

--- components/layout/landing/OgImg.tsx
+++ components/layout/landing/OgImg.tsx
@@ -4,7 +4,24 @@ import React from 'react'
 const OgImg = () => {
   return (
     <div className='flex flex-col border-b border-dashed p-4 w-full'>
-        <Image src="/hero.png" alt='Hero Image' height={1080} width={1920} className='inset-0 rounded-lg mask-r-from-95% mask-l-from-97% mask-b-from-90% mask-t-from-95% dark:mask-none'></Image>
+        <div className="relative">
+
+        <Image
+            src="/hero.png"
+            alt="Hero Image"
+            height={1080}
+            width={1920}
+            className="inset-0 rounded-lg mask-r-from-95% mask-l-from-97% mask-b-from-90% mask-t-from-95% dark:hidden"/>
+
+        <Image
+            src="/hero-dark.png"
+            alt="Hero Image Dark"
+            height={1080}
+            width={1920}
+            className="inset-0 rounded-lg hidden dark:block"/>
+            
+        </div>
+
     </div>
   )
 }
