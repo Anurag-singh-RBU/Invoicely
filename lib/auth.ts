581cb4d1ae433eb9a4a91018e8082ea061ae1ab3
ANURAG SINGH <anuragsingh260805@gmail.com>
2025-12-08 00:17:03 +0530

    Auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,18 +5,23 @@ import { db } from "@/db/drizzle";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-    socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID as string, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-        }, 
+  baseURL: process.env.BETTER_AUTH_URL!,  
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
-    emailAndPassword: { 
-        enabled: true, 
-    }, 
-    database: drizzleAdapter(db, {
-        provider: "pg",
-        schema,
-    }),
-    plugins: [nextCookies()]
+  },
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+
+  plugins: [nextCookies()],
 });
