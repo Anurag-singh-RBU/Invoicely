59ae2effb5597bdde5f2b62bb284f0be66216d14
ANURAG SINGH <anuragsingh260805@gmail.com>
2025-12-08 01:39:05 +0530

    Auth

--- components/sidebar/Auth.tsx
+++ components/sidebar/Auth.tsx
@@ -16,12 +16,12 @@ const Auth = () => {
     return (
       <SidebarMenu>
         <SidebarMenuItem>
-          <div className="bg-muted-foreground/5 flex flex-col gap-1 rounded-lg p-4 shadow-xs">
+          <div className="bg-muted-foreground/5 flex flex-col gap-1 rounded-lg p-4 shadow-xs max-w-[260px]">
             <div className="instrument-serif font-semibold tracking-wider">Login</div>
             <p className="text-muted-foreground text-xs">
               Login to your account to save your data and access your data anywhere
             </p>
-            <div className='w-2 h-auto mt-3'>
+            <div className='w-fit h-auto mt-3'>
               <Link href="/login">
                 <Button variant="default">Login</Button>
               </Link>
@@ -41,7 +41,7 @@ const Auth = () => {
             <Image src={session?.user?.image?.trim() || "/Auth-default.png"} alt="logo" width={38} height={38} className='rounded-md'/>
             <div className='flex flex-col'>
               <span className='instrument-sans truncate font-semibold capitalize' style={{wordSpacing: "3px"}}>{(session.user.name).split("_").slice(-1)[0]}</span>
-              <span className='text-xs geist-sans text-gray-600 tracking-wider max-w-auto truncate block'>{session.user.email}</span>
+              <span className='text-xs geist-sans text-gray-600 tracking-wider sm:max-w-150 max-w-auto truncate block'>{session.user.email}</span>
             </div>
           </div>
           <div className='w-auto h-auto mt-3'>
