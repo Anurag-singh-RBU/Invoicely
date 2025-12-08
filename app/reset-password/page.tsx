e575b3ed5e941faa4d6647f6311806c5265f45da
ANURAG SINGH <anuragsingh260805@gmail.com>
2025-12-08 16:07:19 +0530

    Auth

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,11 @@
+import { ForgotPwdForm } from "@/components/forgotpwd-form"
+
+export default function ForgotPwdPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <ForgotPwdForm/>
+      </div>
+    </div>
+  )
+}
