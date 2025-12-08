ac8ae31f9132c27ae842c99583e2a1608602ce21
ANURAG SINGH <anuragsingh260805@gmail.com>
2025-12-08 16:01:13 +0530

    Auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,15 +6,12 @@ import { nextCookies } from "better-auth/next-js";
 import { Resend } from "resend";
 import ForgotPasswordEmail from "@/components/emails/resetpwd";
 
-// --- Resend ---
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export const auth = betterAuth({
 
-  // MUST match frontend env
-  baseURL: process.env.BETTER_AUTH_URL!, // http://localhost:3000
+  baseURL: process.env.BETTER_AUTH_URL!,  
 
-  // --- Social Login (Google) ---
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -22,37 +19,19 @@ export const auth = betterAuth({
     },
   },
 
-  // --- Email + Password Authentication ---
   emailAndPassword: {
     enabled: true,
-
-    sendResetPasswordEmail: true, // MUST BE TRUE !!!
-
-    // Email Sender Logic
-    sendResetPassword: async ({ user, url }) => {
-
-      // VERY IMPORTANT → MUST "await"
-      const result = await resend.emails.send({
-        from: "Invoicely <onboarding@resend.dev>", // Verified sender
+    sendResetPasswordEmail: true,
+    sendResetPassword: async ({user , url}) => {
+      await resend.emails.send({
+        from: "Invoicely <onboarding@resend.dev>",
         to: user.email,
         subject: "Reset Your Password",
-        react: ForgotPasswordEmail({
-          username: user.name,
-          userEmail: user.email,
-          resetUrl: url,
-          companyName: "Invoicely",
-        }),
-      });
-
-      console.log("RESET PASSWORD EMAIL RESULT:", result);
-
-      if (result.error) {
-        console.error("RESEND ERROR:", result.error);
-      }
-    },
+        react: ForgotPasswordEmail({username: user.name , userEmail: user.email , resetUrl: url , companyName: "Invoicely"})
+      })
+    }
   },
 
-  // --- Database (PostgreSQL + Drizzle) ---
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
@@ -60,6 +39,3 @@ export const auth = betterAuth({
 
   plugins: [nextCookies()],
 });
-
-// Next.js API routes export
-export const { GET, POST } = auth;
